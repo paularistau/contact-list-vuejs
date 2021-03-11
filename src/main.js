027108943194d1328f0e08e8e839477ddb2db82c
Paula Ristau <paularistauf@gmail.com>
2021-03-11 14:05:09 -0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+const expresss = require('express');
+const PORT = 3000
+const HOST = '0.0.0.0';
+
+const app = expresss();
+
+app.get('/', (req, res) => {
+  res.send(App)
+});
+app.listen(PORT, HOST);
+
 Vue.config.productionTip = false;
 
 new Vue({
